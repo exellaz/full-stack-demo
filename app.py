beb2e20ff6fff7fa4ff227a2b2dca1441651d726
Kau Khai Kit <74185102+exellaz@users.noreply.github.com>
2025-11-10 11:33:17 +0800

    add redis based visitor counter

--- app.py
+++ app.py
@@ -1,4 +1,12 @@
 from fastapi import FastAPI
+from redis import Redis
 import os
 
 app = FastAPI()
+
+redis = Redis(host='redis', port=6379)
+
+@app.get("/")
+def read_root():
+    count = redis.incr('hits')
+    return {"message": f"This page has been seen {count} times"}
